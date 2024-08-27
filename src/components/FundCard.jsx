48e127cc11841b5120d9e80f210992f18c555bc9
Arpit Kumar Singh <singharpit302@gmail.com>
2024-08-27 23:26:00 +0530

    home page 1.2

---
+++ src/components/FundCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { tagType, thirdweb } from '../assets'
+import { daysLeft } from '../utils'
+
+const FundCard = ({owner, title, description, target, deadine, amountCollected, image, handleClick}) => {
+  return (
+    <div>
+      FundCard
+    </div>
+  )
+}
+
+export default FundCard

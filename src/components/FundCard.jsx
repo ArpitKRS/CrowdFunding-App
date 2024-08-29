3754d8ebc6b674bafa01029c5bebfee768f74184
Arpit Kumar Singh <singharpit302@gmail.com>
2024-08-29 14:15:50 +0530

    Starting card on home page

--- src/components/FundCard.jsx
+++ src/components/FundCard.jsx
@@ -3,6 +3,7 @@ import { tagType, thirdweb } from '../assets'
 import { daysLeft } from '../utils'
 
 const FundCard = ({owner, title, description, target, deadine, amountCollected, image, handleClick}) => {
+    const remainingDays = daysLeft(deadine)
   return (
     <div>
       FundCard
